b09b43c972a36ad9fee2dd7cb3ad3ed60a3c44f8
sabeela13 <sabeelaa24cse@vetias.ac.in>
2025-07-02 14:57:48 +0530

    arrays

---
+++ arrey.java
@@ -0,0 +1,8 @@
+public class arrey{
+    public static void main(String[]args){
+        int[]marks={80,90,78,78,69,00};
+        for(int mark:marks){
+            System.out.println(mark);
+        }
+    }
+}
