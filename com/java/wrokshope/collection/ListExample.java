95d1cd95a43d28c0cb8742cf1a064ece548af063
sabeela13 <sabeelaa24cse@vetias.ac.in>
2025-07-11 14:56:25 +0530

    ListExample

---
+++ com/java/wrokshope/collection/ListExample.java
@@ -0,0 +1,18 @@
+package com.java.wrokshope.collection;
+import java.util.ArrayList;
+import java.util.List;
+public class ListExample {
+public static void main (String[]args){
+                List<String> names = new ArrayList<>();
+                names.add("keerthi");
+                names.add("ABOO");
+                names.add("nithu");
+                names.add("sabi");
+
+                System.out.println(names);                        
+                name.set(1:"arasu");
+                for(int i=0;<names.size();i++){
+                    System.out.println(names.get(1));
+                }
+                }
+}
