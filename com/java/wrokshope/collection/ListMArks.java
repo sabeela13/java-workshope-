6a08849ac1586d3c6ab744adf7f660090b52cecc
sabeela13 <sabeelaa24cse@vetias.ac.in>
2025-07-11 14:56:36 +0530

    list mark

---
+++ com/java/wrokshope/collection/ListMArks.java
@@ -0,0 +1,19 @@
+package com.java.wrokshope.collection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMArks {
+
+    public static void main(String[]args){
+        List<Integer>marks=new Arraylist<>();
+        marks.add(90);
+        marks.add(78);
+        marks.add(69);
+        marks.add(99);
+        marks.add(80);
+        collections.sort(marks);
+        System.out.println("Sorted List of Marks in ascending");
+        System.out.println(marks);
+        System.out.println("Sorted List of Marks in decending");
+    }
+}
