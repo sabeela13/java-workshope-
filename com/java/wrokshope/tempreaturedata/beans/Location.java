c084b4c3b129d759a1eecabaf0988e748d117b93
sabeela13 <sabeelaa24cse@vetias.ac.in>
2025-07-11 14:57:05 +0530

    tempreture

--- com/java/wrokshope/tempreaturedata/beans/Location.java
+++ com/java/wrokshope/tempreaturedata/beans/Location.java
@@ -14,5 +14,47 @@ public class Location {
     public String getName(){
         return name;
     }
-
+    public void setLatitude(Double aLatitude){
+        latitude=aLatitude;
+    }
+    public Double getLatitude(){
+        return latitude;
+    }
+    public void setLongitude(double aLongitude){
+        longitude=aLongitude;
+    }
+    public double getLongitude(){
+        return longitude;
+    }
+    public void setDescription(String aDescription){
+        description=aDescription;
+    }
+    public void getDescription(){
+        return Description;
+    }
+    public void setCountry(String acountry){
+        country=aCountry;
+    }
+    public void grtCity(String aCity){
+        city=aCity;
+    }
+    public String getCity(){
+        return city;
+    }
+    public void setAddress(String aAddress){
+        address=aAddress;
+    }
+    public String getAddress(){
+        return address;
+    }     
+           public void setPostalcode(String aPostalcode){      
+        Postalcode=aPostalcode;
+    }
+    public String getPostalcode(){
+        return Postalcode;
+    }
+    @Override
+public String toString(){
+    return"Location{ name:" + name+",Latitude:" +Latitude+",Longitude:"+longitude+",
+    Description:"+description+",country:"+country+",city:"+city+",Address:"+addres+",postalcode";}
 }
