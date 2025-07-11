cb8bd794a925b246be136652451f8db1e0b86a1d
sabeela13 <sabeelaa24cse@vetias.ac.in>
2025-07-11 14:56:00 +0530

    name

---
+++ com/java/basic/arrays/NameGenerated.java
@@ -0,0 +1,18 @@
+ package com.java.basic.arrays;
+
+public class NameGenerated {
+    public static void main([]args){
+        scanner input=new scanner(system.in);
+        System.out.println("Enter your name");
+        String name=length.name();
+        System.out.println("Enter your gendre");
+        String gender=length.next();
+        System.out.println("Enter your qualification");
+        String qualification=length.next();
+        StringBuilder sb=new StringBuilder();
+        if "female".equalsIgnoreCase(gendre);
+        {
+            sb.append("miss").append(gendre);
+        }
+    }
+}
