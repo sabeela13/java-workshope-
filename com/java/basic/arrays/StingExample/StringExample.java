33752f1373edba72eb5b185c59980e636c5fec73
sabeela13 <sabeelaa24cse@vetias.ac.in>
2025-07-11 14:56:13 +0530

    StringExample

---
+++ com/java/basic/arrays/StingExample/StringExample.java
@@ -0,0 +1,23 @@
+package com.java.basic.arrays.StringExample;
+
+public class StringExample {
+    public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("java");
+        sb.append(" ").append("is");
+        sb.append(" ");
+        sb.append("the").append(" ");
+        sb.append("most");
+        sb.append(" ").append("used");
+        sb.append(" ");
+        sb.append("language").append(" ");sb.append("for");
+        sb.append(" ").append("enterprise");
+        sb.append(" ");
+        sb.append("application");
+        System.out.println(sb.toString);
+    }
+
+}
+
+}
