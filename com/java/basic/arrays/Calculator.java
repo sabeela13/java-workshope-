0cce844600c795bc2649d4ee490cca0b61773402
sabeela13 <sabeelaa24cse@vetias.ac.in>
2025-07-11 14:55:21 +0530

    calculator

---
+++ com/java/basic/arrays/Calculator.java
@@ -0,0 +1,9 @@
+package com.java.basic.arrays;
+
+public class Calculator {
+    Calculator calculator=new Calculator();
+    ystem.out.println("Division:"+calculator.divide(6,0));
+    System.out.println("Addition:"+calculator.add(5,4));
+    System.out.println("Subraction:"+calculator.subtract(10,4))
+
+}
