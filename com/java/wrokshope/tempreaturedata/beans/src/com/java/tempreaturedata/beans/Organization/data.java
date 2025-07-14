254a445ab95c3ae162c40e872ec490330f171864
sabeela13 <sabeelaa24cse@vetias.ac.in>
2025-07-14 10:27:08 +0530

    data

---
+++ com/java/wrokshope/tempreaturedata/beans/src/com/java/tempreaturedata/beans/Organization/data.java
@@ -0,0 +1,12 @@
+package com.java.wrokshope.tempreaturedata.beans.src.com.java.tempreaturedata.beans.Organization;
+
+public record data(String name,
+    String completeAddress,
+    String website,
+    String email,String contactnumber,
+    long registerationNumber,
+    LocalData registrationData){
+
+    }
+
+
